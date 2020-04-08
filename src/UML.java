520073764a2549426f9135b0a8f187aa4800d759
rick <yuhao8888@gmail.com>
2020-04-08 21:31:54 +0800

    combine different button code into UML code

--- src/UML.java
+++ src/UML.java
@@ -1,8 +1,70 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
 
 public class UML {
 
+	public static class select_button extends JButton {
+		public select_button() {
+			this.ispressed = 0;
+			this.setIcon(icons[ispressed]);
+			this.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					change_icon();
+				}
+			});
+		}
+		
+		public void change_icon() {
+			ispressed = (ispressed + 1)%2;
+			this.setIcon(icons[ispressed]);
+		}
+		
+
+		private ImageIcon []icons = {new ImageIcon("./img/select_icon.png"), new ImageIcon("./img/select_icon_pressed.png")};
+		private int ispressed;
+	}
+	
+	public static class asso_button extends JButton {
+		public asso_button() {
+			ImageIcon icon = new ImageIcon("./img/asso_icon.png");
+			this.setIcon(icon);
+		}
+	}
+	
+	public static class gen_button extends JButton {
+		public gen_button() {
+			ImageIcon icon = new ImageIcon("./img/gen_icon.png");
+			this.setIcon(icon);
+		}
+	}
+	
+	public static class com_button extends JButton {
+		public com_button() {
+			ImageIcon icon = new ImageIcon("./img/com_icon.png");
+			this.setIcon(icon);
+		}
+	}
+
+	public static class class_button extends JButton {
+		public class_button() {
+			ImageIcon icon = new ImageIcon("./img/class_icon.png");
+			this.setIcon(icon);
+		}
+	}
+
+	public static class use_class_button extends JButton {
+		public use_class_button() {
+			ImageIcon icon = new ImageIcon("./img/use_class_icon.png");
+			this.setIcon(icon);
+		}
+	}
+	
+	
+	
 	public static void main(String[] args) {
 		// Java window setup
 		Toolkit tk = Toolkit.getDefaultToolkit();
@@ -16,11 +78,15 @@ public class UML {
 		cp.setLayout(null);
 		
 		JMenu file = new JMenu("File");
-		JMenuItem one = new JMenuItem("one");
-		file.add(one);
+		JMenuItem op = new JMenuItem("Open file");
+		JMenuItem sv = new JMenuItem("Save file");
+		file.add(op);
+		file.add(sv);
 		
 		JMenu edit = new JMenu("Edit");
-		JMenuItem two = new JMenuItem("two");
+		JMenuItem one = new JMenuItem("1");
+		JMenuItem two = new JMenuItem("2");
+		edit.add(one);
 		edit.add(two);
 		
 		JMenuBar menu = new JMenuBar();
@@ -30,15 +96,16 @@ public class UML {
 		
 		int button_height = (int)(xSize*0.1);
 		
-		JButton []buttons = new JButton[6];
-		buttons[0] = new select_button(xSize, 0, button_height*0);
-		buttons[1] = new asso_button(xSize, 0, button_height*1);
-		buttons[2] = new gen_button(xSize, 0, button_height*2);
-		buttons[3] = new com_button(xSize, 0, button_height*3);
-		buttons[4] = new class_button(xSize, 0, button_height*4);
-		buttons[5] = new use_class_button(xSize, 0, button_height*5);
-		
-		for(int i = 0; i < 6; i++) {
+		JButton []buttons = {
+				new select_button(), 
+				new asso_button(), 
+				new gen_button(), 
+				new com_button(), 
+				new class_button(), 
+				new use_class_button()
+		};
+		for(int i = 0; i < buttons.length; i++) {
+			buttons[i].setBounds(0, button_height*i, button_height, button_height);
 			cp.add(buttons[i]);
 		}
 		
--- src/line_button.java
+++
@@ -1,7 +0,0 @@
-import javax.swing.JButton;
-
-public class line_button extends JButton {
-	public line_button(int xSize, int x, int y) {
-		this.setBounds(x, y, (int)(xSize*0.1), (int)(xSize*0.1));
-	}
-}
