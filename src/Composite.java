67165d96dadf561fdd14ddb3fa99bb0d6ce5f8a5
rick <yuhao8888@gmail.com>
2020-06-11 16:01:04 +0800

    upload composite object

--- src/Composite.java
+++ src/Composite.java
@@ -1,41 +1,107 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.awt.Graphics;
+import java.awt.Point;
+import java.util.Vector;
 
-public class Composite{
-	 public Composite(List<Integer> obj_ids, int idx) {
-		// TODO Auto-generated constructor stub
-		 this.data = obj_ids;
-		 this.id = idx;
+public class Composite extends Shape{
+	private Vector<Shape> childs = new Vector<Shape>();
+	
+	public void addchild(Shape o) {
+		childs.add(o);
 	}
-	 public List<Integer> getList(){
-		 return data;
-	 }
-	 public void print_element() {
-		 for(Integer i:data) {
-			 System.out.print((int)i+" ");
-		 }
-	 }
-	 public int get_parent_id() {
-		return parent_id;
+
+	@Override
+	public String gettype() {
+		// TODO Auto-generated method stub
+		return "Composite";
 	}
-	 public void set_parent_id(int i) {
-		 parent_id = i;
-	 }
-	 public void set_id(int i) {
-		id = i;
+	
+	public void draw(Graphics g) {
+		super.draw(g);
+		for(Shape o:childs) {
+			o.draw(g);
+		}
 	}
-	 public int get_id() {
-		 return id;
-	 }
-	 public boolean find_obj_group(int obj_id) {
-		 return data.contains((Integer)(obj_id));
-	 }
-	 
-	 public int get_data_length() {
-		return data.size();
+	
+	public Vector<Shape> getchilds(){return childs;}
+	
+	public void setselect(boolean b) {
+		for(Shape o:childs) {
+			o.setselect(b);
+		}
 	}
-	 
-	 private int parent_id = -1;
-	 private int id = -1;
-	 private List<Integer> data = new ArrayList<Integer>();
+	
+	public boolean isinside(Point p) {
+		for(Shape o:childs) {
+			if(o.isinside(p)) return true;
+		}
+		return false;
+	}
+	
+	public Shape gettop(Point p) {
+		int top_depth = Integer.MIN_VALUE;
+		Shape top_shape = null;
+		for(Shape o:childs) {
+			switch (o.gettype()) {
+			case "RectObject": 
+				if(((RectObject)o).isinside(p)) {
+					if(((RectObject)o).getdepth() > top_depth) {
+						top_depth = ((RectObject)o).getdepth();
+						top_shape = o;
+					}
+				}
+				break;
+			case "Composite":
+				Shape tmp_shape = ((Composite)o).gettop(p);
+				if(tmp_shape != null &&((RectObject)tmp_shape).getdepth() > top_depth) {
+					top_depth = ((RectObject)tmp_shape).getdepth();
+					top_shape = tmp_shape;
+				}
+			}
+		}
+		return top_shape;
+	}
+	
+	public void setposition(Point mouse_start, Point origin, Point mouse_end) {
+		double x_diff = mouse_start.getX() - origin.getX();
+		double y_diff = mouse_start.getY() - origin.getY();
+		Point tmpPoint = null;
+		for(Shape o:childs) {
+			switch (o.gettype()) {
+			case "RectObject":
+				tmpPoint = ((RectObject)o).getposition();
+				break;
+			case "Composite":
+				tmpPoint = origin;
+			}
+			o.setposition(mouse_start, tmpPoint, mouse_end);
+		}
+	}
+	public void updateport(Graphics g) {
+		for(Shape o:childs) {
+			o.updateport(g);
+		}
+	}
+	
+	public boolean isinarea(Point start, Point end) {
+		boolean all_true = true;
+		for(Shape o:childs) {
+			if(!o.isinarea(start, end)) {
+				all_true = false;
+				break;
+			}
+		}
+		return all_true;
+	}
+	
+	public boolean isselect() {
+		boolean all_true = true;
+		for(Shape o:childs) {
+			if(!o.isselect()) {
+				all_true = false;
+				break;
+			}
+		}
+		return all_true;
+	}
+	
 }
