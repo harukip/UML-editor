f3ca1777dff539124764ee7735802696f12f7aba
rick <yuhao8888@gmail.com>
2020-06-11 16:02:01 +0800

    update changename menu

---
+++ src/ChangeName.java
@@ -0,0 +1,31 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Vector;
+
+import javax.swing.JMenuItem;
+
+public class ChangeName extends JMenuItem implements ActionListener {
+	private Menu menu;
+	
+	public ChangeName(Menu m) {
+		menu = m;
+		this.setText("Change Object Name");
+		this.addActionListener(this);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		Canvas canvas = menu.getCanvas();
+		Vector<Shape> objs = canvas.getObjects();
+		int select_count = 0, obj_index = -1;
+		for(int i = 0; i < objs.size(); i++) {
+			if(objs.elementAt(i).isselect()) {
+				select_count += 1;
+				obj_index = i;
+			}
+		}
+		if(select_count == 1) {
+			if(objs.elementAt(obj_index).gettype().equals("RectObject")) {new ChangeNameWindow(canvas, obj_index);}
+		}
+	}
+}
